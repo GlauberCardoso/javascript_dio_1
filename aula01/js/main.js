f13fdda8173b7507ced2e210974830b90fbcce49
GlauberCardoso <glauberdevs@gmail.com>
2023-02-17 12:45:17 -0300

    Aula 01 - JavaScript - Dio

---
+++ aula01/js/main.js
@@ -0,0 +1,21 @@
+var nome = "Glauber Cardoso"
+//console.log(nome)
+var idade = 32
+var idade2 = 10
+var frase = "Japão é a melhor seleção do mundo!"
+console.log(frase)
+//alert("Olá, " + nome + "! Seja Bem-Vindo!!! Você tem " + idade + " anos.")
+
+//alert(idade + idade2)
+
+//console.log(idade + idade2)
+console.log("Japão??? Mudando a seleção usando .replace: ")
+console.log(frase.replace("Japão", "Brasil"))
+console.log("AGORA SIM :)")
+console.log("Vamos Gritar isso usando .toUpperCase(): ")
+console.log(frase.toUpperCase())
+console.log("Ops, Japão??? Vamos Gritar Brasil!!!")
+console.log(frase.replace("Japão", "Brasil").toUpperCase())
+console.log("agora ficou lindo!!! :)".toUpperCase()) //Deixa tudo MAIÚSCULO!!!
+console.log("ESSA FRASE NÃO PRECISA FICAR GRITANDO, VAMOS DIMINUIR USANDO .toLowerCase(): ")
+console.log("ESSA FRASE NÃO PRECISA FICAR GRITANDO, POR ISSO EU DIMINUI!!!".toLowerCase())
